0aa15885f2653bd00ff91f0c78a8243e89ef5e17
kogo <30936224+saba161@users.noreply.github.com>
2023-06-26 09:59:07 +0000

    add operator overload examples

--- Exercise/Exercise.CSharp/Program.cs
+++ Exercise/Exercise.CSharp/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hi");
+﻿ThreeD a = new ThreeD(1, 2, 3);
+ThreeD b = new ThreeD(10, 10, 10);
+ThreeD c;
+
+Console.WriteLine("Coordinats point a: ");
+a.Show();
+Console.WriteLine();
+
+Console.WriteLine("Coordinats point b: ");
+b.Show();
+Console.WriteLine();
+
+c = a + b;
+c.Show();
+Console.WriteLine();
+
+
+c = a + b + c;
+c.Show();
+Console.WriteLine();
