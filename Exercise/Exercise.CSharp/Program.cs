fd3e4c8f9237ff21ed13b8b6beb2945a64f11244
kogo <saba.koguashvili1999@mail.ru>
2023-07-11 22:07:12 +0400

    comment

--- Exercise/Exercise.CSharp/Program.cs
+++ Exercise/Exercise.CSharp/Program.cs
@@ -1,7 +1,21 @@
 ﻿
+using System.Linq.Expressions;
 using Exercise.CSharp.StructAndClass;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
 
 ExecuteMyStractAndClass s = new ExecuteMyStractAndClass();
 
 s.ExecuteStruct();
 s.ExecuteClass();
+
+//
+//
+// new List<int>().Where(a =>
+//
+//     new DbSet().Where()
+//
+//
+// Expression<Func<int, int, int>> expr;
+// Func<int, int, int> f;
+// f()
