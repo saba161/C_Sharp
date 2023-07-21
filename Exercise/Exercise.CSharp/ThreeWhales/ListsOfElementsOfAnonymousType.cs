2a5a8abd712eabc7b1597d8a782b11b896a8128b
kogo <saba.koguashvili1999@mail.ru>
2023-07-21 22:45:45 +0400

    ThreeWhales

--- Exercise/Exercise.CSharp/ThreeWhales/ListsOfElementsOfAnonymousType.cs
+++ Exercise/Exercise.CSharp/ThreeWhales/ListsOfElementsOfAnonymousType.cs
@@ -4,7 +4,7 @@ public class ListsOfElementsOfAnonymousType
 {
     public void ListOfElementsWithANonymousType()
     {
-        //a list of elements of an anonymous type
+        //a list of elements of an anonymous type 
         List<int> list = new List<int>();
         var x = new { Name = "Saba", Age = 24 };
 
