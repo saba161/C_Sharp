97393e6d31c734ed1d6e87fe68f585ac3377b0ea
kogo <saba.koguashvili1999@mail.ru>
2023-08-08 21:08:58 +0400

    namespaces

---
+++ Exercise/Exercise.CSharp/Namespaces/Count.cs
@@ -0,0 +1,19 @@
+namespace Counter
+{
+    public class CountDown
+    {
+        public void Count()
+        {
+        }
+    }
+};
+
+namespace Counter2
+{
+    public class CountDown
+    {
+        public void Count()
+        {
+        }
+    }
+}
---
+++ Exercise/Exercise.CSharp/Namespaces/Execute.cs
@@ -0,0 +1,10 @@
+namespace Exercise.CSharp.Namespaces;
+
+public class Execute
+{
+    public void ExecuteCounter()
+    {
+        Counter.CountDown countOne = new Counter.CountDown();
+        Counter2.CountDown countTwo = new Counter2.CountDown();
+    }
+}
