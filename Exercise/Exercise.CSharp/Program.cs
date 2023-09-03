24c22b5030da7367cdd615934c8e001b0f642b17
kogo <saba.koguashvili1999@mail.ru>
2023-09-03 18:24:53 +0400

    exerice

--- Exercise/Exercise.CSharp/Program.cs
+++ Exercise/Exercise.CSharp/Program.cs
@@ -1,15 +1,41 @@
-﻿string str = "123456";
+﻿//{ { "aaa" }, { "bbb" }, { "ccc" } } => { { "aaa" }, { "bbb" }, { "ccc" } }
+//{ { "aaa", "bbb" }, { "aaa", "bbb", "ccc" }, { "aaa", "bbb", "ccc", "ddd" } } => { { "aaa", "bbb" }, { "ccc" }, { "ddd" } }
+//{ { "aaa-bbb", "ccc-aaa" }, { " bbb-ddd ", "ddd - aaa", " eee - fff " } }
+//  => { { "aaa", "bbb", "ccc" }, { "ddd", "eee", "fff" } }
 
-float number;
-
-try
+static string[][] GetUniqueWordsArray(IEnumerable<string> lines, char separator)
 {
-    number = float.Parse(str);
-    Console.WriteLine(number);
+    List<string[]> result = new List<string[]>();
+    List<string> uniqueWordsSet = new List<string>();
 
+    foreach (string line in lines)
+    {
+        string[] words = line.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+        List<string> uniqueWords = new List<string>();
+
+        foreach (string word in words)
+        {
+            string trimmedWord = word.Trim();
+            // Check if the word is not already in the uniqueWordsSet
+            if (!uniqueWordsSet.Contains(trimmedWord))
+            {
+                uniqueWordsSet.Add(trimmedWord);
+                uniqueWords.Add(trimmedWord);
+            }
+        }
+        
+        if (uniqueWords.Count > 0)
+        {
+            result.Add(uniqueWords.ToArray());
+        }
+    }
+
+    return result.ToArray();
 }
-catch (FormatException)
-{
-    number = -6;
-    Console.WriteLine(number);
-}
+
+string[] inputLines = new string[] { "aaa-bbb-aaa", "ccc-aaa", " bbb-ddd ", "ddd - aaa", " eee - fff " };
+char separator = '-';
+
+var result = GetUniqueWordsArray(inputLines, separator);
+
+var s = 5 + 5;
